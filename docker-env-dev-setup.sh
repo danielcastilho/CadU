3a0bda3cba9c00be4a4b4e999cc1fa46c2027d22
Daniel Castilho <danielcastilho74@gmail.com>
2020-11-26 13:53:48 -0300

    fix sh script

--- docker-env-dev-setup.sh
+++ docker-env-dev-setup.sh
@@ -2,6 +2,6 @@
 docker pull postgres
 docker pull dpage/pgadmin4
 docker network create --driver bridge cadu
-docker run --name cadu-postgres --network=cadu -e "POSTGRES_PASSWORD=CadU2020!" -p 5432:5432 -v /home/daniel/Desenvolvimento/PostgreSQL:/var/lib/postgresql/data -d postgres
-docker run --name cadu-pgadmin --network=cadu -p 15432:80 -e "PGADMIN_DEFAULT_EMAIL=danielcastilho74@gmail.com" -e "PGADMIN_DEFAULT_PASSWORD=CadU2020!" -d dpage/pgadmin4
+docker run --name cadu-postgres --network=cadu -e "POSTGRES_PASSWORD=CadU2020!" -p 5432:5432 -v ~/Desenvolvimento/PostgreSQL:/var/lib/postgresql/data -d postgres
+docker run --name cadu-pgadmin --network=cadu -p 15432:80 -e "PGADMIN_DEFAULT_EMAIL=mail@gmail.com" -e "PGADMIN_DEFAULT_PASSWORD=CadU2020!" -d dpage/pgadmin4
 docker ps
